7ad1b3dd0e05dfe77b476b728d6238413563f6c2
Thyon3 <asnakemengesha80@gmail.com>
2026-02-28 23:46:52 -0500

    Update rating model schema

--- server/src/model/rating.ts
+++ server/src/model/rating.ts
@@ -3,9 +3,11 @@ import { Schema } from "mongoose";
 export interface IRating {
   userId: string;
   rating: number;
+  review?: string;
 }
 
 export const ratingSchema = new Schema<IRating>({
   userId: { type: String, required: true },
   rating: { type: Number, required: true },
+  review: { type: String, trim: true },
 });
