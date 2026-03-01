bf7bb0052c0d1d368b4483b910c5128bbafc2a8c
Thyon3 <asnakemengesha80@gmail.com>
2026-02-28 23:45:17 -0500

    Add account repository interface

--- amazoneclonefrontend/lib/features/account/domain/repositories/account_repository.dart
+++ amazoneclonefrontend/lib/features/account/domain/repositories/account_repository.dart
@@ -1,11 +1,14 @@
+import 'package:flutter_amazon_clone_bloc/features/auth/domain/entities/user.dart';
 import '../../../../core/utils/typedef.dart';
 import '../../../product/domain/entities/product_entity.dart';
 
 abstract class AccountRepository {
   // Browsing History
   ResultFuture<List<ProductEntity>> getBrowsingHistory();
-  
+
   ResultVoid clearBrowsingHistory();
-  
+
   ResultVoid addToBrowsingHistory(String productId);
+
+  ResultFuture<User> updateProfile({String? name, String? email});
 }
